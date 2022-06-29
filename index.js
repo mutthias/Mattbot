7a06aa96c15d499b4721ffd649564800007f5ab7
Matt <98845506+mutthias@users.noreply.github.com>
2022-06-28 23:54:13 -0700

    redid json and index
    redid the json and index.js files cuz the original tutorial was not working

--- index.js
+++ index.js
@@ -1,17 +1,22 @@
 const {Client, Intents, Message} = require('discord.js');
 const {token} = require('./config.json');
 
-const client = new Client({intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]});
+const client = new Client({
+  intents: [
+    "GUILDS",
+    "GUILD_MESSAGES"
+  ]
+});
 
 client.once('ready', () => {
-  console.log('Ready!');
+  console.log(`Logged in as ${client.user.tag}`);
 });
 
 client.on('message', (message) => {
   if (message.author.id === client.user.id) return;
 
   if (message.content === "ping") {
-    message.reply("boy shutcho squiggly diggly ass up");
+    message.reply("boy shutcho squiggly diggly ass up twerk");
   }
 });
 
