7be686da54b0e1022fc9ab4e4acb179abac74b2b
Matt <98845506+mutthias@users.noreply.github.com>
2022-06-30 00:15:14 -0700

    trying to write commands but once again something isnt working

--- index.js
+++ index.js
@@ -1,35 +1,53 @@
-const {Client, Intents, Message} = require('discord.js');
+const Discord = require('discord.js');
 require("dotenv").config();
 
 const generateImage = require("./generateimage.js");
 
-const client = new Client({
+const client = new Discord.Client({
   intents: [
     "GUILDS",
-    "GUILD_MESSAGES"
+    "GUILD_MESSAGES",
+    "GUILD_MEMBERS"
   ]
 });
 
-client.once('ready', () => {
-  console.log(`Logged in as ${client.user.tag}`);
-});
+let bot = {
+  client,
+  prefix: "n.",
+  owners: ["582376952980045842"]
+}
 
-client.on('message', (message) => {
-  if (message.author.id === client.user.id) return;
+client.commands = new Discord.Collection();
+client.events = new Discord.Collection();
 
-  if (message.content === "ping") {
-    message.reply("boy shutcho squiggly diggly ass up twerk");
-  }
-});
+client.loadEvents = (bot, reload) => require("./handlers/events")(bot, reload);
+client.loadCommands = (bot, reload) => require("./handlers/commands")(bot, reload)
 
-const welcomeChannelID = "991602566066602056";
+client.loadEvents(bot, false);
+client.loadCommands(bot, false);
 
-client.on('guildMemberAdd', async (member) => {
-  const img = await generateImage(member);
-  member.guild.channels.cache.get(welcomeChannelID).send({
-    content:`<@${member.id}> Welcome to da server`,
-    files: [img]
-  })
-})
+module.exports = bot;
+
+// client.once('ready', () => {
+//   console.log(`Logged in as ${client.user.tag}`);
+// });
+
+// client.on('message', (message) => {
+//   if (message.author.id === client.user.id) return;
+
+//   if (message.content === "ping") {
+//     message.reply("boy shutcho squiggly diggly ass up twerk");
+//   }
+// });
+
+// const welcomeChannelID = "991602566066602056";
+
+// client.on('guildMemberAdd', async (member) => {
+//   const img = await generateImage(member);
+//   member.guild.channels.cache.get(welcomeChannelID).send({
+//     content:`<@${member.id}> Welcome to da server`,
+//     files: [img]
+//   })
+// })
 
 client.login(process.env.TOKEN);
