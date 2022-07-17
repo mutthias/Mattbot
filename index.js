9a49f1e3a4f88bfbafcc6b59531b2cdf44a03859
Matt <98845506+mutthias@users.noreply.github.com>
2022-07-17 12:37:54 -0700

    loop command
    fixed loop command as well as a bit of cleanup code

--- index.js
+++ index.js
@@ -92,30 +92,3 @@ client.loadCommands(bot, false);
 module.exports = bot
 
 client.login(process.env.TOKEN);
-
-
-
-
-
-
-// client.once('ready', () => {
-//   console.log(`Logged in as ${client.user.tag}`);
-// });
-
-// client.on('message', (message) => {
-//   if (message.author.id === client.user.id) return;
-
-//   if (message.content === "ping") {
-//     message.reply("boy shutcho squiggly diggly ass up twerk");
-//   }
-// });
-
-// const welcomeChannelID = "991602566066602056";
-
-// client.on('guildMemberAdd', async (member) => {
-//   const img = await generateImage(member);
-//   member.guild.channels.cache.get(welcomeChannelID).send({
-//     content:`<@${member.id}> Welcome to da server`,
-//     files: [img]
-//   })
-// })
